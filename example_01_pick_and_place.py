5a3c9f9fcd526989b7381ec370c35f9e33597cb7
Alexandre Stueben <Alexandre.Stueben@mailbox.org>
2018-11-12 16:34:44 +0100

    Fixed some unused variable. Changed move_joints_collision_free to move_joints.

--- example_01_pick_and_place.py
+++ example_01_pick_and_place.py
@@ -2,13 +2,9 @@ import signal
 import functools
 import asyncio
 
-import numpy as np
-
-from pyquaternion import Quaternion
-
 from xamla_motion.world_view_client import WorldViewClient
-from xamla_motion.motion_client import MoveGroup, EndEffector
-from xamla_motion.data_types import JointValues, Pose
+from xamla_motion.motion_client import MoveGroup
+from xamla_motion.data_types import JointValues
 from xamla_motion.gripper_client import WeissWsgGripperProperties, WeissWsgGripper
 from xamla_motion.robot_chat_client import RobotChatClient, RobotChatSteppedMotion
 
@@ -34,9 +30,6 @@ def main(loopCount: int):
     # create move group instance targeting the right arm of the robot
     move_group = MoveGroup("/sda10f/right_arm_torso")
 
-    # get the corresponding end effector
-    end_effector = move_group.get_end_effector()
-
     # get the gripper attached at the right arm
     wsg_gripper = get_gripper(move_group)
 
@@ -61,7 +54,7 @@ def main(loopCount: int):
 
     async def move_supervised(joint_values: JointValues, velocity_scaling=1):
         """Opens a window in rosvita to let the user supervise the motion to joint values """
-        stepped_client = move_group.move_joints_collision_free_supervised(joint_values, 
+        stepped_client = move_group.move_joints_supervised(joint_values, 
                                                         velocity_scaling = velocity_scaling)
         robot_chat_stepped_motion = RobotChatSteppedMotion(robot_chat,
                                                         stepped_client,
@@ -78,27 +71,27 @@ def main(loopCount: int):
     async def go_to_prepick():
         print("Go to prepick position and open gripper")
         # Allows parallel adjustment of the gripper and movement of the end effector
-        t1 = move_group.move_joints_collision_free(jv_prePick, velocity_scaling=0.5)
+        t1 = move_group.move_joints(jv_prePick, velocity_scaling=0.5)
         t2 = wsg_gripper.grasp(0.02, 1, 0.05 )
         await asyncio.gather(t1, t2)
 
     async def pick_up():
         """Simple example of a "picking up" motion """
         print("Pick up")
-        await move_group.move_joints_collision_free(jv_pick, velocity_scaling=0.04)
+        await move_group.move_joints(jv_pick, velocity_scaling=0.04)
         await wsg_gripper.grasp(0.002, 0.1, 0.05)
-        await move_group.move_joints_collision_free(jv_prePick)
+        await move_group.move_joints(jv_prePick)
 
     async def go_to_preplace():
         print("Go to preplace position")
-        await move_group.move_joints_collision_free(jv_prePlace)   
+        await move_group.move_joints(jv_prePlace)   
 
     async def place():
         """Simple example of a "placing" motion """
         print("Place")
-        await move_group.move_joints_collision_free(jv_place, velocity_scaling=0.04)
+        await move_group.move_joints(jv_place, velocity_scaling=0.04)
         await wsg_gripper.grasp(0.04, 1, 0.05)
-        await move_group.move_joints_collision_free(jv_prePlace)  
+        await move_group.move_joints(jv_prePlace)  
 
     async def pick_and_place():
         """Actions to be repeated in loop"""
