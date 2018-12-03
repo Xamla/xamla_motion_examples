3ffe78d691853a5b2e4e8d775a3158735617513b
Alexandre Stueben <Alexandre.Stueben@mailbox.org>
2018-12-03 16:41:59 +0100

    Added first tinkering with jogging in example 07 and a first draft of a jogging client

---
+++ example_07/example_07_jogging.py
@@ -0,0 +1,65 @@
+"""
+This example shows how to rotate the torso joint while keeping the end effectors
+at certain poses 
+"""
+import numpy as np
+import math
+
+import time 
+
+import copy
+
+import asyncio
+import signal
+import functools
+
+from xamla_motion.world_view_client import WorldViewClient
+
+from xamla_motion.data_types import Pose, JointSet, JointValues, JointPath
+from xamla_motion.motion_client import MoveGroup
+
+
+import example_utils
+
+from example_07.jogging_client import JoggingClient
+
+from xamla_motion.utility import register_asyncio_shutdown_handler 
+
+import rospy 
+import actionlib
+
+            
+
+def main():
+    world_view_folder = ""
+    joggingClient = JoggingClient()
+
+    world_view_client = WorldViewClient()
+    move_group = example_utils.get_move_group()
+    current_pose = move_group.get_end_effector().compute_pose(move_group.get_current_joint_positions())
+
+    move_group_name = "/sda10f/right_arm_torso"
+    joggingClient.set_move_group_name(move_group_name)
+    joggingClient.toggle_tracking(True)
+
+
+    running = True
+    for i in range(1000):    
+        
+        pose = current_pose
+
+        delta = [0,0,0]
+        pose = pose.translate(delta)
+        pose = world_view_client.get_pose("Pose_1", world_view_folder)
+
+        joggingClient.send_set_point(pose)
+        time.sleep(0.02)
+
+
+    # Stop tracking
+    joggingClient.toggle_tracking(False)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ example_07/jogging_client.py
@@ -0,0 +1,85 @@
+
+import rospy
+import actionlib
+
+from datetime import timedelta
+
+from xamla_motion.data_types import Pose, JointValues
+from geometry_msgs.msg import PoseStamped
+from trajectory_msgs.msg import JointTrajectoryPoint
+from xamlamoveit_msgs.srv import SetString
+
+from std_srvs.srv import SetBool
+
+from xamla_motion.utility import ROSNodeSteward
+
+
+class JoggingClient(object):
+
+    __setpoint_topic = "/xamlaJointJogging/jogging_setpoint"
+    
+    __set_move_group_service_name = "/xamlaJointJogging/set_movegroup_name"
+    __toggle_tracking_service_name = "xamlaJointJogging/start_stop_tracking"
+
+    def __init__(self):
+        self.__ros_node_steward = ROSNodeSteward()
+        self._create_connect_topics()
+        self._create_connect_services()
+
+
+    def _create_connect_topics(self):
+        self._set_point = rospy.Publisher(self.__setpoint_topic, 
+                                        PoseStamped,
+                                        queue_size=5)
+
+    def _create_connect_services(self):
+        try:
+            self.__set_move_group_service = rospy.ServiceProxy(
+                self.__set_move_group_service_name,
+                SetString)
+        except rospy.ServiceException as exc:
+            raise ServiceException('connection for service with name: ' +
+                                   self.__set_move_group_service_name +
+                                   ' could not be established') from exc
+
+        try:
+            self.__toggle_tracking_service = rospy.ServiceProxy(
+                self.__toggle_tracking_service_name,
+                SetBool)
+        except rospy.ServiceException as exc:
+            raise ServiceException('connection for service with name: ' +
+                                   self.__toggle_tracking_service_name +
+                                   ' could not be established') from exc
+
+
+    def send_set_point(self, setPoint: Pose):
+        pose_msg  = setPoint.to_posestamped_msg() 
+        print("publish pose")
+        self._set_point.publish(pose_msg)
+
+
+    def send_velocities(self, velocities: JointValues):
+        point = JointTrajectoryPoint(
+            time_from_start = timedelta(seconds=0.008),
+            velocities = velocities.values)
+            
+
+
+    def set_move_group_name(self, name: str):
+        try:
+            response = self.__set_move_group_service(name)
+        except rospy.ServiceException as exc:
+            raise ServiceException('service call for query'
+                                   ' set move group'
+                                   ' failed, abort') from exc
+        print(response)
+
+
+    def toggle_tracking(self, toggle: bool):
+        try:
+            response = self.__toggle_tracking_service(toggle)
+        except rospy.ServiceException as exc:
+            raise ServiceException('service call for query'
+                                   ' set toggle'
+                                   ' failed, abort') from exc
+        print(response)
