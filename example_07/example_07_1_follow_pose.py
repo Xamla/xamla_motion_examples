4992571f98df4c6c80819cf40744f3ef3ee73d27
Alexandre Stueben <Alexandre.Stueben@mailbox.org>
2018-12-05 15:18:28 +0100

    Refactored first example

--- example_07/example_07_1_follow_pose.py
+++ example_07/example_07_1_follow_pose.py
@@ -11,6 +11,39 @@ from xamla_motion.world_view_client import WorldViewClient
 import example_utils
 from example_07.jogging_client import JoggingClient
 
+
+
+def track_point(time_amount: float, 
+                frequency: float,  
+                point_name: str,
+                get_pose,
+                jogging_client: JoggingClient) -> None:
+    """ 
+    Tracks a point 
+
+    Parameters
+    ----------
+    time_amount : float
+        Amount of time the tracking should occur
+    frequency : float
+        Frequency of the calls to JoggingClient in Hz
+    get_pose : lambda function
+        Function which returns a Pose when called
+    jogging_client : JoggingClient
+        The JoggingClient
+    """
+
+    # Calculate the number of calls to jogging client based on frequency and time
+    N = int(time_amount*frequency)
+    print(N)
+    for i in range(N):    
+        print("Call {} of {}".format(i+1, N))
+        # Read the current Pose from WorldView
+        pose = get_pose()
+        jogging_client.send_set_point(pose)
+        # Send set point in a frequency of 50 Hz, so that the arm keeps moving
+        time.sleep(1/frequency)
+
 def main():
     world_view_folder = "example_07_jogging"
 
@@ -23,23 +56,24 @@ def main():
 
     current_pose = move_group.get_end_effector().compute_pose(move_group.get_current_joint_positions())
 
+    point_name = "TrackingPose"
     # Write pose to World View, so the tracking begins with the current end effector pose
-    world_view_client.update_pose( "TrackingPose",  world_view_folder, current_pose)
+    world_view_client.update_pose( point_name,  world_view_folder, current_pose)
+    
+
+    get_pose = lambda : world_view_client.get_pose(point_name, world_view_folder)
+
     #Begin tracking
     jogging_client.toggle_tracking(True)
-
-    for i in range(1000):    
-        print("{} of {}".format(i, 1000))
-        # Read the current Pose from WorldView
-        pose = world_view_client.get_pose("TrackingPose", world_view_folder)
-
-        jogging_client.send_set_point(pose)
-        # Send set point in a frequency of 50 Hz, so that the arm keeps moving
-        time.sleep(0.02)
+    # Track for 20 seconds with a frequency of 50 Hz
+    track_point(time_amount=20, 
+                frequency=50, 
+                point_name=point_name, 
+                get_pose=get_pose,
+                jogging_client=jogging_client)
 
     # Stop tracking
     jogging_client.toggle_tracking(False)
 
 if __name__ == '__main__':
     main()
-
