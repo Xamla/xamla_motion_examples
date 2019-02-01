1a8148268c9f3f7e571ff052e63ff2f5c085b0e3
Alexandre Stueben <Alexandre.Stueben@mailbox.org>
2019-02-01 17:37:26 +0100

    Little timer hack to avoid having jogging error cluttering console

--- example_07/example_07_jogging_feedback.py
+++ example_07/example_07_jogging_feedback.py
@@ -2,7 +2,14 @@
 This module contain some utility functions to get and format jogging feedback
 """
 
+import threading
+
+
 from xamla_motion.jogging_client import JoggingClient, JoggingClientFeedbackState, JoggingErrorCode
+
+msg = ""
+timer = threading.Timer(1.0, lambda x : None)
+blind = False
 
 def callback_function(state: JoggingClientFeedbackState):
     """
@@ -10,9 +17,24 @@ def callback_function(state: JoggingClientFeedbackState):
 
     In the context of this example, we just print the error code.
     """
+    def callback():
+        """ resets blind to false"""
+        global blind
+        blind = False
 
+    global msg
+    global timer
+    global blind
     # only print the error code when it is not OK
     if not state.error_code ==  JoggingErrorCode.OK:
-        print("Jogging Error occurred: {}".format(state.error_code))
+        new_msg = "Jogging Error occurred: {}".format(state.error_code)
+        if not new_msg == msg or not blind:
+            msg = new_msg
+            print(msg)
+            blind  = True
+            timer = threading.Timer(1.0, callback)
+            timer.start()
 
+            
+                
 
